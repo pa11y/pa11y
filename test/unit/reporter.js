4cae0631870889edaeaaf521d814522e79e49a49
Rowan Manning <rowan.manning@nature.com>
2013-06-24 15:53:15 +0100

    Sanitize reporters to reduce empty exported functions

--- test/unit/reporter.js
+++ test/unit/reporter.js
@@ -4,42 +4,67 @@
 
 var assert = require('proclaim');
 var mockery = require('mockery');
+var sinon = require('sinon');
 
 describe('reporter', function () {
 	var reporter = require('../../lib/reporter');
 
-	beforeEach(function () {
-		mockery.enable({
-			warnOnUnregistered: false,
-			warnOnReplace: false
+	describe('.sanitize()', function () {
+
+		it('should default all reporter functions', function () {
+			var rep = reporter.sanitize({});
+			assert.isFunction(rep.begin);
+			assert.isFunction(rep.log);
+			assert.isFunction(rep.debug);
+			assert.isFunction(rep.error);
+			assert.isFunction(rep.handleResult);
+			assert.isFunction(rep.end);
 		});
-		mockery.registerMock('pa11y-reporter-foo', {});
+
 	});
 
-	afterEach(function () {
-		mockery.disable();
-	});
+	describe('.load()', function () {
+		var fooReporter;
 
-	describe('#load()', function () {
+		beforeEach(function () {
+			mockery.enable({
+				warnOnUnregistered: false,
+				warnOnReplace: false
+			});
+			fooReporter = {};
+			mockery.registerMock('pa11y-reporter-foo', fooReporter);
+			sinon.spy(reporter, 'sanitize');
+		});
+
+		afterEach(function () {
+			mockery.disable();
+			reporter.sanitize.restore();
+		});
 
 		it('should load built-in reporters', function () {
-			reporter.load('json', function (err, reporter) {
+			reporter.load('json', function (err, rep) {
 				assert.isNull(err);
-				assert.isObject(reporter);
+				assert.isObject(rep);
 			});
 		});
 
 		it('should load reporters in `pa11y-reporter-<name>` node modules', function () {
-			reporter.load('foo', function (err, reporter) {
+			reporter.load('foo', function (err, rep) {
 				assert.isNull(err);
-				assert.isObject(reporter);
+				assert.isObject(rep);
 			});
 		});
 
 		it('should error when no reporter is found', function () {
-			reporter.load('please-dont-register-this-module-name', function (err, reporter) {
+			reporter.load('please-dont-register-this-module-name', function (err, rep) {
 				assert.isObject(err);
-				assert.isNull(reporter);
+				assert.isNull(rep);
+			});
+		});
+
+		it('should sanitize the loaded reporter', function () {
+			reporter.load('foo', function () {
+				assert.isTrue(reporter.sanitize.withArgs(fooReporter).calledOnce);
 			});
 		});
 
