e6efeab0d3a8140d1c36749718aa2388a1e84419
Rowan Manning <rowan.manning@nature.com>
2013-07-12 09:55:28 +0100

    Shift some responsibility off of the command line tool

--- test/unit/pa11y.js
+++ test/unit/pa11y.js
@@ -11,4 +11,51 @@ describe('pa11y', function () {
 		assert.isObject(pa11y);
 	});
 
+    it('should have an applyDefaultOpts function', function () {
+        assert.isFunction(pa11y.applyDefaultOpts);
+    });
+
+    it('should have a sniff function', function () {
+        assert.isFunction(pa11y.sniff);
+    });
+
+    describe('.applyDefaultOpts()', function () {
+
+        it('should return the expected object when no options are set', function () {
+            assert.deepEqual(pa11y.applyDefaultOpts({}), {
+                debug: false,
+                htmlcs: 'http://squizlabs.github.io/HTML_CodeSniffer/build/HTMLCS.js',
+                reporter: 'console',
+                standard: 'WCAG2AA',
+                timeout: 30000
+            });
+        });
+
+        it('should return the expected object when all options are set', function () {
+            var opts = {
+                debug: true,
+                htmlcs: 'foo',
+                reporter: 'bar',
+                standard: 'baz',
+                timeout: 123
+            };
+            assert.deepEqual(pa11y.applyDefaultOpts(opts), opts);
+        });
+
+        it('should return the expected object when some options are set', function () {
+            assert.deepEqual(pa11y.applyDefaultOpts({
+                debug: true,
+                reporter: 'foo',
+                timeout: 123
+            }), {
+                debug: true,
+                htmlcs: 'http://squizlabs.github.io/HTML_CodeSniffer/build/HTMLCS.js',
+                reporter: 'foo',
+                standard: 'WCAG2AA',
+                timeout: 123
+            });
+        });
+
+    });
+
 });
