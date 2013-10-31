f1ae0cf5ad0cb27617b30292a4cb8cc776e2e5f0
Rowan Manning <rowan.manning@nature.com>
2013-10-31 11:56:23 +0000

    Tabs, not spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,48 +1,54 @@
 module.exports = function (grunt) {
 
-    grunt.initConfig({
+	grunt.initConfig({
 
-        cucumberjs: {
-            src: 'test/feature',
-            options: {
-                format: 'pretty'
-            }
-        },
+		cucumberjs: {
+			src: 'test/feature',
+			options: {
+				format: 'pretty'
+			}
+		},
 
-        jshint: {
-            all: ['bin/pa11y', 'Gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
-            options: {
-                es3: false,
-                indent: 4,
-                latedef: false,
-                maxcomplexity: 5,
-                maxdepth: 2,
-                maxlen: 100,
-                maxparams: 4,
-                maxstatements: 12,
-                node: true,
-                quotmark: 'single'
-            }
-        },
+		jshint: {
+			all: ['bin/pa11y', 'Gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
+			options: {
+				es3: false,
+				indent: 4,
+				latedef: false,
+				maxcomplexity: 5,
+				maxdepth: 2,
+				maxlen: 100,
+				maxparams: 4,
+				maxstatements: 12,
+				node: true,
+				quotmark: 'single'
+			}
+		},
 
-        mochaTest: {
-            test: {
-                src: ['test/unit/**/*.js'],
-                options: {
-                    reporter: 'spec'
-                }
-            }
-        }
+		mochaTest: {
+			unit: {
+				src: ['test/unit/**/*.js'],
+				options: {
+					reporter: 'spec'
+				}
+			},
+			functional: {
+				src: ['test/functional/*.js'],
+				options: {
+					reporter: 'spec'
+				}
+			}
+		}
 
-    });
+	});
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-cucumber');
-    grunt.loadNpmTasks('grunt-mocha-test');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-cucumber');
+	grunt.loadNpmTasks('grunt-mocha-test');
 
-    grunt.registerTask('lint', ['jshint']);
-    grunt.registerTask('test', ['mochaTest', 'cucumberjs']);
-    grunt.registerTask('default', ['lint', 'test']);
-    grunt.registerTask('ci', ['lint', 'mochaTest']);
+	grunt.registerTask('lint', ['jshint']);
+	grunt.registerTask('test', ['mochaTest:unit', 'mochaTest:functional']);
+	grunt.registerTask('default', ['lint', 'test']);
+	grunt.registerTask('ci', ['lint', 'mochaTest']);
 
 };
