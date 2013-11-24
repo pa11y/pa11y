6697a39af97ca690262306ecbd4833e48f3dabd4
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2013-11-24 10:37:04 -0330

    Better test for searching PATH.

--- test/unit/search-path.js
+++ test/unit/search-path.js
@@ -1,20 +1,52 @@
-/* global describe, it */
+/* global afterEach, beforeEach, describe, it */
 'use strict';
 
 var assert = require('proclaim');
+var mockery = require('mockery');
 
 describe('searchPath()', function () {
-	var searchPath = require('../../lib/search-path');
+	var searchPath;
+	beforeEach(function () {
+		mockery.enable({
+			useCleanCache: true,
+			warnOnUnregistered: false,
+			warnOnReplace: false
+		});
+		mockery.registerMock('path', {
+			'sep': '/',
+			'delimiter': ':'
+		});
+		mockery.registerMock('fs', {
+			existsSync: function (filename) {
+				switch (filename) {
+					case '/usr/bin/bar':
+					case '/opt/local/bin/phantomjs':
+						return true;
+					default:
+						return false;
+				}
+			}
+		});
+		searchPath = require('../../lib/search-path');
+	});
+	afterEach(function () {
+		mockery.disable();
+	});
 	it('should be a function', function () {
 		assert.isFunction(searchPath);
 	});
-	it('should not find foo, bar, or baz', function () {
-		searchPath(['foo', 'bar', 'baz'], function (found) {
+	it('should find bar', function () {
+		searchPath('/usr/local/bin:/usr/bin:/bin', ['foo', 'bar', 'baz'], function (found) {
+			assert.isTrue(found);
+		});
+	});
+	it('should not find tcsh or Bash', function () {
+		searchPath('/usr/local/bin:/usr/bin:/bin', ['csh', 'bash'], function (found) {
 			assert.isFalse(found);
 		});
 	});
 	it('should find PhantomJS', function () {
-		searchPath(['phantomjs', 'phantomjs.exe'], function (found) {
+		searchPath('/opt/local/bin', ['phantomjs'], function (found) {
 			assert.isTrue(found);
 		});
 	});
