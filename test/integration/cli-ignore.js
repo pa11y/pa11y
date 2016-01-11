27aaf0912aba29fd22b89e03f45b727ee8b6d1b8
Rowan Manning <rowan.manning@nature.com>
2016-01-11 10:53:44 +0000

    Write a test case for the multiple ignore option

--- test/integration/cli-ignore.js
+++ test/integration/cli-ignore.js
@@ -43,6 +43,40 @@ describe('Pa11y CLI Ignore', function() {
 
 	});
 
+	describeCliCall('/errors', ['--ignore', 'warning', '--ignore', 'WCAG2AA.Principle3.Guideline3_1.3_1_1.H57.2'], {}, function() {
+
+		it('should respond with an exit code of `0`', function() {
+			assert.strictEqual(this.lastExitCode, 0);
+		});
+
+		it('should respond with the expected messages', function() {
+			assert.isArray(this.lastJsonResponse);
+			assert.lengthEquals(this.lastJsonResponse, 1);
+			assert.deepEqual(this.lastJsonResponse[0], {
+				code: 'WCAG2AA.Principle2.Guideline2_4.2_4_2.H25.2',
+				context: '<title>Page Title</title>',
+				message: 'Check that the title element describes the document.',
+				selector: 'html > head > title',
+				type: 'notice',
+				typeCode: 3
+			});
+		});
+
+	});
+
+	describeCliCall('/errors', ['--ignore', 'warning', '--ignore', 'WCAG2AA.Principle3.Guideline3_1.3_1_1.H57.2;WCAG2AA.Principle2.Guideline2_4.2_4_2.H25.2'], {}, function() {
+
+		it('should respond with an exit code of `0`', function() {
+			assert.strictEqual(this.lastExitCode, 0);
+		});
+
+		it('should respond with the expected messages', function() {
+			assert.isArray(this.lastJsonResponse);
+			assert.lengthEquals(this.lastJsonResponse, 0);
+		});
+
+	});
+
 	describeCliCall('/errors', ['--config', './mock/config/ignore.json'], {}, function() {
 
 		it('should respond with an exit code of `0`', function() {
