1b1d870f2873d5d532faf4a31274f0d88ed72799
Glynn Phillips <glynndominicphillips@gmail.com>
2016-05-19 16:28:54 +0100

    Fixes the hide-elements options
    The `hide-elements` options was broken due to a misunderstanding of how
    `HTML_codeSniffer` worked.
    
    This change moves away from relying on `HTML_codeSniffer` for this
    feature and instead filters out errors that are unwanted.

--- test/integration/cli-hide-elements.js
+++ test/integration/cli-hide-elements.js
@@ -7,18 +7,30 @@ var describeCliCall = require('./helper/describe-cli-call');
 
 describe('Pa11y CLI Hide Elements', function() {
 
-	describeCliCall('/hide-elements', ['--hide-elements', '.heading'], {}, function() {
+	describeCliCall('/hide-elements', ['--hide-elements', '#hide-parent', '--ignore', 'warning;notice'], {}, function() {
 
 		it('should respond with an exit code of `0`', function() {
 			assert.strictEqual(this.lastExitCode, 0);
 		});
 
-		it('should respond with the expected messages', function() {
+		it('should respond with no errors', function() {
 			assert.isArray(this.lastJsonResponse);
-			assert.strictEqual(this.lastJsonResponse.length, 3);
-			assert.notStrictEqual(this.lastJsonResponse[0].type, 'error');
-			assert.notStrictEqual(this.lastJsonResponse[1].type, 'error');
-			assert.notStrictEqual(this.lastJsonResponse[2].type, 'error');
+			assert.strictEqual(this.lastJsonResponse.length, 0);
+		});
+
+	});
+
+	describeCliCall('/hide-elements', ['--hide-elements', 'h1,h2,img,iframe,#para,form,input', '--ignore', 'warning;notice'], {}, function() {
+
+		it('should respond with an exit code of `2`', function() {
+			assert.strictEqual(this.lastExitCode, 2);
+		});
+
+		it('should respond with the visible elements error messages', function() {
+			assert.isArray(this.lastJsonResponse);
+			assert.strictEqual(this.lastJsonResponse.length, 2);
+			assert.strictEqual(this.lastJsonResponse[0].context.match(/^(<h1|<h2|<img|<iframe|id="para|<form|<input")/), null);
+			assert.strictEqual(this.lastJsonResponse[1].context.match(/^(<h1|<h2|<img|<iframe|id="para|<form|<input")/), null);
 		});
 
 	});
