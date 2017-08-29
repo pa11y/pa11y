dbfc486895fd8a2644dcb44afd07b20e4346e24d
Rowan Manning <accounts@rowanmanning.co.uk>
2017-08-29 10:26:09 +0100

    Switch from PhantomJS to Headless Chrome (#304)
    * Get Pa11y working with headless Chrome
    
    This doesn't complete the required testing or linting, and there will
    probably need to be some refactoring around this.
    
    * Fix lint errors in the lib folder
    
    * Update unit tests for the action module
    
    * Update unit tests for the runner module
    
    * Add tests for actions in the pa11y main library
    
    * Re-alias the isValidAction method
    
    * Add a missing item from the migration list
    
    * Add a chromeLaunchConfig option
    
    * Fix lint errors in the reporters
    
    * Update the examples to use the new API
    
    * Fix lint errors in the bin folder
    
    * Fix a few broken reporter tests
    
    * Fix all lint errors in the tests
    
    * Write a proper migration guide (this is a WIP)
    
    * Update the troubleshooting guide
    
    * Update the README with all the work in this PR
    
    * Fix the screencapture integration tests
    
    * Update package-lock.json
    
    * Update docs based on Hollie's feedback
    
    * Use strict mode everywhere again
    
    * Remove the reliance on a full scheme in the URL
    
    * Confirm removal of the HTML CodeSniffer option

--- reporter/json.js
+++ reporter/json.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var bfj = require('bfj');
+const bfj = require('bfj');
 
 module.exports = {
 	begin: emptyFunction,
@@ -18,11 +18,11 @@ function reportError(message) {
 }
 
 function reportResults(results) {
-	var stream = bfj.streamify(results);
-	stream.on('dataError', function(error) {
+	const stream = bfj.streamify(results);
+	stream.on('dataError', error => {
 		reportError(error.message);
 	});
-	stream.on('end', function() {
+	stream.on('end', () => {
 		process.stdout.write('\n');
 	});
 	stream.pipe(process.stdout);
