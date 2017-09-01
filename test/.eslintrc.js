66196d4e6b3403867a78f51527d066c6432281f9
Rowan Manning <rowan.manning@ft.com>
2017-09-01 16:18:07 +0100

    Tighten up the lint rules for tests now that we can

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -3,12 +3,6 @@
 // Clone the main config
 const config = module.exports = JSON.parse(JSON.stringify(require('../.eslintrc')));
 
-// We use `this` all over the integration tests
-config.rules['no-invalid-this'] = 'off';
-
 // Disable max line length/statements
 config.rules['max-len'] = 'off';
 config.rules['max-statements'] = 'off';
-
-// We use non arrow functions all over the tests for now
-config.rules['prefer-arrow-callback'] = 'off';
