8725b83eaad0184734b76a89fbfe185a456749b0
Rowan Manning <accounts@rowanmanning.co.uk>
2017-09-04 10:53:40 +0100

    Integration test overhaul (#312)
    * Remove the current integration tests
    
    * Test that the CLI exit codes are correct
    
    * Test the environment flag
    
    * Test that the CLI output is as expected
    
    * Test that the standard flag works
    
    * Test that the ignore CLI flag works
    
    * Add comments to mock HTML files
    
    * Test that the CLI root-element flag works
    
    * Test that the CLI hide-elements flag works
    
    * Test that the CLI screen-capture flag works
    
    * Test that CLI wait and timeout work as expected
    
    * Test the CLI add-rule flag
    
    * Test that Pa11y issues have the correct selectors
    
    * Test the config CLI flag with a few config files
    
    * Fix some lint errors in the new tests
    
    * Write integration tests for the reporters
    
    * Test that click, input, and check actions work
    
    * Test that the wait-for-url actions work
    
    * Test that the wait-for-element-state actions work
    
    * Tighten up the lint rules for tests now that we can
    
    * Add comments to some of the more confusing tests

---
+++ test/integration/cli/ignore.js
@@ -0,0 +1,99 @@
+'use strict';
+
+const assert = require('proclaim');
+const runPa11yCli = require('../helper/pa11y-cli');
+
+// Note: we use the JSON reporter in here to make it easier
+// to inspect the output issues. The regular CLI output is
+// tested in the reporter tests
+describe('CLI ignore', () => {
+	let pa11yResponse;
+
+	describe('when the `--ignore` flag is set to "warning"', () => {
+
+		before(async () => {
+			pa11yResponse = await runPa11yCli(`${global.mockWebsiteAddress}/errors`, {
+				arguments: [
+					'--ignore', 'warning',
+					'--reporter', 'json'
+				]
+			});
+		});
+
+		it('ignores warnings', () => {
+			assert.isArray(pa11yResponse.json);
+			assert.lengthEquals(pa11yResponse.json, 2);
+			pa11yResponse.json.forEach(issue => {
+				assert.notStrictEqual(issue.type, 'warning');
+			});
+		});
+
+	});
+
+	describe('when the `--ignore` flag is set to "warning;notice"', () => {
+
+		before(async () => {
+			pa11yResponse = await runPa11yCli(`${global.mockWebsiteAddress}/errors`, {
+				arguments: [
+					'--ignore', 'warning;notice',
+					'--reporter', 'json'
+				]
+			});
+		});
+
+		it('ignores warnings and notices', () => {
+			assert.isArray(pa11yResponse.json);
+			assert.lengthEquals(pa11yResponse.json, 1);
+			pa11yResponse.json.forEach(issue => {
+				assert.notStrictEqual(issue.type, 'warning');
+				assert.notStrictEqual(issue.type, 'notice');
+			});
+		});
+
+	});
+
+	describe('when the `--ignore` flag is set multiple times', () => {
+
+		before(async () => {
+			pa11yResponse = await runPa11yCli(`${global.mockWebsiteAddress}/errors`, {
+				arguments: [
+					'--ignore', 'warning',
+					'--ignore', 'notice',
+					'--reporter', 'json'
+				]
+			});
+		});
+
+		it('ignores all of the flagged items', () => {
+			assert.isArray(pa11yResponse.json);
+			assert.lengthEquals(pa11yResponse.json, 1);
+			pa11yResponse.json.forEach(issue => {
+				assert.notStrictEqual(issue.type, 'warning');
+				assert.notStrictEqual(issue.type, 'notice');
+			});
+		});
+
+	});
+
+	describe('when the `--ignore` flag is set to an issue code', () => {
+
+		before(async () => {
+			pa11yResponse = await runPa11yCli(`${global.mockWebsiteAddress}/errors`, {
+				arguments: [
+					'--ignore', 'WCAG2AA.Principle3.Guideline3_1.3_1_1.H57.2',
+					'--reporter', 'json'
+				]
+			});
+		});
+
+		it('ignores issues with the given code', () => {
+			assert.isArray(pa11yResponse.json);
+			assert.lengthEquals(pa11yResponse.json, 2);
+			pa11yResponse.json.forEach(issue => {
+				assert.notStrictEqual(issue.code, 'WCAG2AA.Principle3.Guideline3_1.3_1_1.H57.2');
+			});
+		});
+
+	});
+
+});
---
+++ test/integration/helper/pa11y-cli.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const extend = require('node.extend');
+const spawn = require('child_process').spawn;
+const path = require('path');
+
+module.exports = runPa11yCli;
+
+function runPa11yCli(url, options = {}) {
+
+	// Default the options
+	options = extend(true, {}, {
+		arguments: [],
+		environment: {
+			PATH: process.env.PATH
+		},
+		workingDirectory: path.resolve(`${__dirname}/..`)
+	}, options);
+	options.arguments.push(url);
+
+	return new Promise((resolve, reject) => {
+		const binPath = path.resolve(`${__dirname}/../../../bin/pa11y.js`);
+
+		const response = {
+			exitCode: '',
+			json: null,
+			output: '',
+			stderr: '',
+			stdout: ''
+		};
+
+		const pa11yProcess = spawn(binPath, options.arguments, {
+			cwd: options.workingDirectory,
+			env: options.environment
+		});
+
+		pa11yProcess.stdout.on('data', data => {
+			response.stdout += data;
+			response.output += data;
+		});
+
+		pa11yProcess.stderr.on('data', data => {
+			response.stderr += data;
+			response.output += data;
+		});
+
+		pa11yProcess.on('close', code => {
+			response.exitCode = code;
+			try {
+				response.json = JSON.parse(response.output);
+			} catch (error) {}
+			resolve(response);
+		});
+
+		pa11yProcess.on('error', reject);
+
+	});
+}
