df90db888cdc4ace372bb8c07fbee7dcc33d0182
Rowan Manning <accounts+github@rowanmanning.com>
2018-01-30 11:38:33 +0000

    Use the latest version of the CLI reporter (#374)
    This fixes #373 and adds in a test to ensure it won't regress.

---
+++ test/integration/issue/373.test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const assert = require('proclaim');
+const runPa11yCli = require('../helper/pa11y-cli');
+
+// Note: this test is designed to replicate issue
+// 373, where Pa11y outputs a TypeError if the HTML
+// lang attribute is empty.
+//
+// https://github.com/pa11y/pa11y/issues/373
+describe('Issue #373', () => {
+	let pa11yResponse;
+
+	before(async () => {
+		pa11yResponse = await runPa11yCli(`${global.mockWebsiteAddress}/issue/373`, {
+			arguments: [
+				'--reporter', 'cli'
+			]
+		});
+	});
+
+	describe('CLI output', () => {
+
+		it('does not contain a TypeError exception', () => {
+			assert.notMatch(pa11yResponse.output, /typeerror: cannot read property 'replace' of null/i);
+		});
+
+	});
+
+
+});
