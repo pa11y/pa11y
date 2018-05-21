5b87a269816149e5ef9ea8d271e1bac52ec59c04
Rowan Manning <accounts+github@rowanmanning.com>
2018-05-21 11:47:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,74 @@
+
+# Pa11y aXe Runner
+
+An [aXe](https://www.axe-core.org/) runner for [Pa11y](https://github.com/pa11y/pa11y).
+
+**:warning: This runner is not in use yet, and is here for review purposes. A later version of Pa11y will include this :warning:**
+
+[![NPM version][shield-npm]][info-npm]
+[![Build status][shield-build]][info-build]
+[![LGPL-3.0 licensed][shield-license]][info-license]
+
+
+## Table Of Contents
+
+- [Requirements](#requirements)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+
+## Requirements
+
+The Pa11y aXe Runner is compatible with Pa11y 5.0. It will not work with older versions of Pa11y.
+
+
+## Usage
+
+Install Pa11y and the Pa11y aXe Runner with [npm](https://www.npmjs.com/) (locally or globally is fine):
+
+```sh
+npm install -g pa11y pa11y-runner-axe
+```
+
+Run Pa11y using the aXe runner:
+
+```sh
+pa11y --runner axe http://example.com
+```
+
+
+## Contributing
+
+There are many ways to contribute to the Pa11y aXe Runner, we cover these in the [contributing guide](CONTRIBUTING.md) for this repo.
+
+If you're ready to contribute some code, clone this repo locally and commit your code on a new branch.
+
+Please write unit tests for your code, and check that everything works by running the following before opening a <abbr title="pull request">PR</abbr>:
+
+```sh
+make ci
+```
+
+You can also run verifications and tests individually:
+
+```sh
+make verify              # Verify all of the code (ESLint)
+make test                # Run all tests
+make test-unit           # Run the unit tests
+make test-unit-coverage  # Run the unit tests with coverage
+```
+
+
+## License
+
+The Pa11y aXe Runner is licensed under the [Lesser General Public License (LGPL-3.0)][info-license].<br/>
+Copyright &copy; 2018, Team Pa11y
+
+
+[info-license]: LICENSE
+[info-npm]: https://www.npmjs.com/package/pa11y-runner-axe
+[info-build]: https://travis-ci.org/pa11y/pa11y-runner-axe
+[shield-license]: https://img.shields.io/badge/license-LGPL%203.0-blue.svg
+[shield-npm]: https://img.shields.io/npm/v/pa11y-runner-axe.svg
+[shield-build]: https://img.shields.io/travis/pa11y/pa11y-runner-axe/master.svg
---
+++ test/unit/lib/runner.test.js
@@ -0,0 +1,229 @@
+'use strict';
+
+const assert = require('proclaim');
+const sinon = require('sinon');
+const path = require('path');
+
+describe('lib/runner', () => {
+	let result;
+	let originalWindow;
+	let runner;
+
+	beforeEach(() => {
+
+		result = {
+			violations: [
+				{
+					id: 'mock-id-1',
+					description: 'mock description 1',
+					impact: 'mock impact 1',
+					help: 'mock help 1',
+					helpUrl: 'mock-help-url-1',
+					nodes: [
+						{
+							target: [
+								'mock-selector-1'
+							]
+						}
+					]
+				},
+				{
+					id: 'mock-id-2',
+					description: 'mock description 2',
+					impact: 'mock impact 2',
+					help: 'mock help 2',
+					helpUrl: 'mock-help-url-2',
+					nodes: [
+						{
+							target: [
+								'mock-selector-2'
+							]
+						}
+					]
+				}
+			],
+			incomplete: [
+				{
+					id: 'mock-id-3',
+					description: 'mock description 3',
+					impact: 'mock impact 3',
+					help: 'mock help 3',
+					helpUrl: 'mock-help-url-3',
+					nodes: [
+						{
+							target: [
+								'mock-selector-3'
+							]
+						}
+					]
+				},
+				{
+					id: 'mock-id-4',
+					description: 'mock description 4',
+					impact: 'mock impact 4',
+					help: 'mock help 4',
+					helpUrl: 'mock-help-url-4',
+					nodes: [
+						{
+							target: [
+								'mock-selector-4'
+							]
+						}
+					]
+				}
+			]
+		};
+
+		originalWindow = global.window;
+		global.window = {
+			axe: {
+				run: sinon.stub().resolves(result)
+			},
+			document: {
+				querySelector: sinon.stub()
+			}
+		};
+
+		global.window.document.querySelector.withArgs('mock-selector-1').returns('mock-element-1');
+		global.window.document.querySelector.withArgs('mock-selector-2').returns('mock-element-2');
+		global.window.document.querySelector.withArgs('mock-selector-3').returns('mock-element-3');
+		global.window.document.querySelector.withArgs('mock-selector-4').returns('mock-element-4');
+
+		runner = require('../../../lib/runner');
+	});
+
+	afterEach(() => {
+		global.window = originalWindow;
+	});
+
+	it('is an object', () => {
+		assert.isObject(runner);
+	});
+
+	it('has a `supports` property set to a string', () => {
+		assert.isString(runner.supports);
+	});
+
+	it('has a `scripts` property set to an array of scripts the runner is dependent on', () => {
+		assert.isArray(runner.scripts);
+		assert.deepEqual(runner.scripts, [
+			`${path.resolve(__dirname, '..', '..', '..', 'lib')}/../node_modules/axe-core/axe.min.js`
+		]);
+	});
+
+	it('has a `run` method', () => {
+		assert.isFunction(runner.run);
+	});
+
+	describe('.run(options, pa11y)', () => {
+		let options;
+		let pa11y;
+		let resolvedValue;
+
+		beforeEach(async () => {
+			options = {};
+			pa11y = {
+				getElementContext: sinon.stub().returns('mock-element-context'),
+				getElementSelector: sinon.stub().returns('mock-element-selector')
+			};
+			resolvedValue = await runner.run(options, pa11y);
+		});
+
+		it('runs aXe', () => {
+			assert.calledOnce(global.window.axe.run);
+			assert.calledWithExactly(global.window.axe.run);
+		});
+
+		it('generates context for each issue element', () => {
+			assert.callCount(pa11y.getElementContext, 4);
+			assert.calledWithExactly(pa11y.getElementContext.getCall(0), 'mock-element-1');
+			assert.calledWithExactly(pa11y.getElementContext.getCall(1), 'mock-element-2');
+			assert.calledWithExactly(pa11y.getElementContext.getCall(2), 'mock-element-3');
+			assert.calledWithExactly(pa11y.getElementContext.getCall(3), 'mock-element-4');
+		});
+
+		it('generates selectors for each issue element', () => {
+			assert.callCount(pa11y.getElementSelector, 4);
+			assert.calledWithExactly(pa11y.getElementSelector.getCall(0), 'mock-element-1');
+			assert.calledWithExactly(pa11y.getElementSelector.getCall(1), 'mock-element-2');
+			assert.calledWithExactly(pa11y.getElementSelector.getCall(2), 'mock-element-3');
+			assert.calledWithExactly(pa11y.getElementSelector.getCall(3), 'mock-element-4');
+		});
+
+		it('resolves with processed and normalised issues', () => {
+			assert.deepEqual(resolvedValue, [
+				{
+					code: 'mock-id-1',
+					message: 'mock description 1',
+					type: 'error',
+					context: 'mock-element-context',
+					selector: 'mock-element-selector',
+					runnerExtras: {
+						impact: 'mock impact 1',
+						help: 'mock help 1',
+						helpUrl: 'mock-help-url-1'
+					}
+				},
+				{
+					code: 'mock-id-2',
+					message: 'mock description 2',
+					type: 'error',
+					context: 'mock-element-context',
+					selector: 'mock-element-selector',
+					runnerExtras: {
+						impact: 'mock impact 2',
+						help: 'mock help 2',
+						helpUrl: 'mock-help-url-2'
+					}
+				},
+				{
+					code: 'mock-id-3',
+					message: 'mock description 3',
+					type: 'warning',
+					context: 'mock-element-context',
+					selector: 'mock-element-selector',
+					runnerExtras: {
+						impact: 'mock impact 3',
+						help: 'mock help 3',
+						helpUrl: 'mock-help-url-3'
+					}
+				},
+				{
+					code: 'mock-id-4',
+					message: 'mock description 4',
+					type: 'warning',
+					context: 'mock-element-context',
+					selector: 'mock-element-selector',
+					runnerExtras: {
+						impact: 'mock impact 4',
+						help: 'mock help 4',
+						helpUrl: 'mock-help-url-4'
+					}
+				}
+			]);
+		});
+
+		describe('when aXe errors', () => {
+			let axeError;
+			let rejectedError;
+
+			beforeEach(async () => {
+				axeError = new Error('axe error');
+				window.axe.run.reset();
+				window.axe.run.rejects(axeError);
+				try {
+					await runner.run(options, pa11y);
+				} catch (error) {
+					rejectedError = error;
+				}
+			});
+
+			it('rejects with the aXe error', () => {
+				assert.strictEqual(rejectedError, axeError);
+			});
+
+		});
+
+	});
+
+});
