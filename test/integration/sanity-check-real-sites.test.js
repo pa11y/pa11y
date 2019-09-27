9219a7c48d22ad1fe039a10ac87d1bffb05e4c0f
Joey Ciechanowicz <joeyciechanowicz@users.noreply.github.com>
2019-09-27 16:19:03 +0100

    Adds sanity checks against real sites

---
+++ test/integration/sanity-check-real-sites.test.js
@@ -0,0 +1,34 @@
+const assert = require('proclaim');
+const pa11y = require('../../lib/pa11y');
+
+async function runTest(url) {
+	try {
+		const result = await pa11y(url);
+		assert.isObject(result);
+	} catch (error) {
+		assert.fail(`Expected no error but got "${error.message}"`);
+	}
+}
+
+describe('sanity checks', () => {
+
+	it('runs on https://www.nature.com/', async () => {
+		await runTest('https://www.nature.com/');
+	}).timeout(10000);
+
+	it('runs on https://google.com/', async () => {
+		await runTest('https://www.nature.com/');
+	}).timeout(10000);
+
+	it('runs on http://culturaestero.regione.emilia-romagna.it/it', async () => {
+		await runTest('http://culturaestero.regione.emilia-romagna.it/it');
+	}).timeout(10000);
+
+	it('runs on https://www.dundee.ac.uk', async () => {
+		await runTest('https://www.dundee.ac.uk');
+	}).timeout(10000);
+
+	it('runs on https://link.springer.com', async () => {
+		await runTest('https://link.springer.com');
+	}).timeout(10000);
+});
