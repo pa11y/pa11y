cb3cda38d80d71c03f7fbc6a007af8c541326a2e
Joey Ciechanowicz <joeyciechanowicz@users.noreply.github.com>
2019-09-30 11:10:34 +0100

    Removes site and ups the timeout to 15 seconds

--- test/integration/sanity-check-real-sites.test.js
+++ test/integration/sanity-check-real-sites.test.js
@@ -1,38 +1,35 @@
 const assert = require('proclaim');
 const pa11y = require('../../lib/pa11y');
 
+const TIMEOUT = 15000;
+
 async function runTest(url) {
 	try {
 		const result = await pa11y(url);
 		assert.isObject(result);
 	} catch (error) {
-		assert.fail(`Expected no error but got "${error.message}"`);
+		assert.fail(`Expected no error but pa11y threw an exception with message: "${error.message}"`);
 	}
 }
 
-describe('sanity checks', () => {
-
+describe('Pa11y runs without failure', () => {
 	it('runs on https://www.nature.com/', async () => {
 		await runTest('https://www.nature.com/');
-	}).timeout(10000);
+	}).timeout(TIMEOUT);
 
 	it('runs on https://google.com/', async () => {
 		await runTest('https://www.nature.com/');
-	}).timeout(10000);
-
-	it('runs on http://culturaestero.regione.emilia-romagna.it/it', async () => {
-		await runTest('http://culturaestero.regione.emilia-romagna.it/it');
-	}).timeout(10000);
+	}).timeout(TIMEOUT);
 
 	it('runs on https://www.dundee.ac.uk', async () => {
 		await runTest('https://www.dundee.ac.uk');
-	}).timeout(10000);
+	}).timeout(TIMEOUT);
 
 	it('runs on https://link.springer.com', async () => {
 		await runTest('https://link.springer.com');
-	}).timeout(10000);
+	}).timeout(TIMEOUT);
 
 	it('runs on https://www.bbc.co.uk', async () => {
 		await runTest('https://www.bbc.co.uk');
-	}).timeout(10000);
+	}).timeout(TIMEOUT);
 });
