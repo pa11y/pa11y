b557f487eb8316001083f6e9bc14b00ce4058388
Danyal Aytekin <danyal@alienpaper.com>
2024-03-18 17:55:38 +0000

    Make `373`'s test objects more descriptive and minimal, invert assertion and broaden

--- test/integration/issue/373.test.js
+++ test/integration/issue/373.test.js
@@ -3,29 +3,13 @@
 const assert = require('proclaim');
 const runPa11yCli = require('../helper/pa11y-cli');
 
-// Note: this test is designed to replicate issue
-// 373, where Pa11y outputs a TypeError if the HTML
-// lang attribute is empty.
-//
-// https://github.com/pa11y/pa11y/issues/373
-describe('Issue #373', function() {
-	let pa11yResponse;
-
-	before(async function() {
-		pa11yResponse = await runPa11yCli(`${global.mockWebsiteAddress}/issue/373`, {
-			arguments: [
-				'--reporter', 'cli'
-			]
-		});
+describe('pa11y/pa11y#373: With empty HTML lang attribute', () => {
+	it('CLI invocation produces results, avoiding exit with TypeError or any error', async function() {
+		const {output} = await runPa11yCli(
+			`${global.mockWebsiteAddress}/issue/373-html-emptylang-typeerror`, {
+				arguments: ['--reporter', 'cli']
+			}
+		);
+		assert.match(output, /Results for URL/i);
 	});
-
-	describe('CLI output', function() {
-
-		it('does not contain a TypeError exception', function() {
-			assert.notMatch(pa11yResponse.output, /typeerror: cannot read property 'replace' of null/i);
-		});
-
-	});
-
-
 });
