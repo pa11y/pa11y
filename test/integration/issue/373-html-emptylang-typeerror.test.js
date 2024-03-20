e9f8e77de5aa6e291e5ad43a726e10a4ade57515
Danyal Aytekin <danyal@alienpaper.com>
2024-03-20 18:43:37 +0000

    Convert the two Jest tests to Mocha

--- test/integration/issue/373-html-emptylang-typeerror.test.js
+++ test/integration/issue/373-html-emptylang-typeerror.test.js
@@ -3,7 +3,7 @@
 const assert = require('proclaim');
 const runPa11yCli = require('../helper/pa11y-cli');
 
-describe('pa11y/pa11y#373: With empty HTML lang attribute', () => {
+describe('pa11y/pa11y#373: With empty HTML lang attribute', function() {
 	it('CLI invocation produces results, avoiding exit with TypeError or any error', async function() {
 		const {output} = await runPa11yCli(
 			`${global.mockWebsiteAddress}/issue/373-html-emptylang-typeerror`, {
--- test/integration/issue/691-input-autocomplete-username.test.js
+++ test/integration/issue/691-input-autocomplete-username.test.js
@@ -1,14 +1,15 @@
 'use strict';
 
+const assert = require('proclaim');
 const runPa11yCli = require('../helper/pa11y-cli');
 
-describe('pa11y/pa11y#691: With input[autocomplete*=username]', () => {
-	it('CLI invocation produces results, avoiding exit with TypeError', async () => {
+describe('pa11y/pa11y#691: With <input autocomplete="username">, omitting type', function() {
+	it('CLI invocation produces results, avoiding exit with TypeError', async function() {
 		const {output} = await runPa11yCli(
 			`${global.mockWebsiteAddress}/issue/691-input-autocomplete-username`, {
 				arguments: ['--reporter', 'cli']
 			}
 		);
-		expect(output).toMatch(/Results for URL/i);
+		assert.match(output, /Results for URL/i);
 	});
 });
