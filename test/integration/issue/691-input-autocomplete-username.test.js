af5e2713b558e9482496db668fad094a0e126f76
Danyal Aytekin <danyal@alienpaper.com>
2024-03-20 18:46:13 +0000

    Improve the description of 691's test case

--- test/integration/issue/691-input-autocomplete-username.test.js
+++ test/integration/issue/691-input-autocomplete-username.test.js
@@ -3,7 +3,7 @@
 const assert = require('proclaim');
 const runPa11yCli = require('../helper/pa11y-cli');
 
-describe('pa11y/pa11y#691: With <input autocomplete="username">, omitting type', function() {
+describe('pa11y/pa11y#691: With <input autocomplete="username"> (implicit type)', function() {
 	it('CLI invocation produces results, avoiding exit with TypeError', async function() {
 		const {output} = await runPa11yCli(
 			`${global.mockWebsiteAddress}/issue/691-input-autocomplete-username`, {
