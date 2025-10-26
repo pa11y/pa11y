9c4ff008f76323bd7c6d0a07a5c51ce17221f3e2
Danyal Aytekin <danyal@fubase.com>
2025-10-26 21:15:36 +0000

    Tidy single test

--- test/integration/cli/runner-axe.test.js
+++ test/integration/cli/runner-axe.test.js
@@ -3,10 +3,9 @@
 const assert = require('proclaim');
 const runPa11yCli = require('../helper/pa11y-cli');
 
-describe('CLI runner aXe', function() {
-
-	before(async function() {
-		pa11yResponse = await runPa11yCli(`${global.mockWebsiteAddress}/errors`, {
+describe('CLI runner axe', function() {
+	it('outputs the expected issues', async function() {
+		const pa11yResponse = await runPa11yCli(`${global.mockWebsiteAddress}/errors`, {
 			arguments: [
 				'--runner', 'axe',
 				'--reporter', 'json',
@@ -15,7 +14,6 @@ describe('CLI runner aXe', function() {
 			]
 		});
 
-	it('outputs the expected issues', function() {
 		assert.isArray(pa11yResponse.json);
 		assert.lengthEquals(pa11yResponse.json, 3);
 		assert.equal(pa11yResponse.json[0].type, 'error');
